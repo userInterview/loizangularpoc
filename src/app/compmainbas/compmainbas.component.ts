bcd719a2622113abc0840a13c7368b5daa84ac1e
Loizani <Loizani@aol.com>
2022-01-22 19:12:54 +0100

    one way binding click event et ngStyle property

--- src/app/compmainbas/compmainbas.component.ts
+++ src/app/compmainbas/compmainbas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -10,9 +10,51 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class CompmainbasComponent implements OnInit {
 
-  constructor() { }
+  i : number ;
+  j : number ;
+  contenu : string ;
+   /*fromcomp: string ;
+  fromhtml: string ;
+  fromboth: string ;*/
+
+  @Input()
+  attrbut : string ;
+
+  constructor() { 
+
+  }
 
   ngOnInit() {
+    this.i = 0 ;
+    this.j = 0 ;
+  }
+
+  cliquer():void {
+    this.i = this.i+1 ;
+    console.log("<cliquer()> : vous avez cliqué " + this.i + "fois !!") ;
+  }
+
+  setattr():string {
+    this.j = this.j + 1 ;
+    this.attrbut = "attribut \"attrbut\" initialisé " + this.j +" fois" ;
+    console.log(this.attrbut) ;
+    return this.attrbut
+  }
+
+
+  setInnerHtml() :void {
+  this.contenu = "variable membre \"contenu\" initialise attribut du DOM \"innerHTML\" par <i>attribute binding</i> (du model vers le template)" ;
+  }
+
+  readstyle():void {
+    var objdiv = document.getElementById("mondiv") ;
+    var attrStyle = objdiv.getAttribute("style") ;
+    console.log(attrStyle.toString()) ;
+}
+
+  membstyle : object ;
+  setMemBStyle():void {
+    this.membstyle = {"color":"blue"} ;
   }
 
 }
