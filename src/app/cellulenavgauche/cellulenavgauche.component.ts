3d48fd934951fd1304408896990888c760fdb12c
Loizani <Loizani@aol.com>
2022-02-04 20:12:44 +0100

    Preparation implementation menu gauche : placement sur le noeud listemenugauche du json parametre

--- src/app/cellulenavgauche/cellulenavgauche.component.ts
+++ src/app/cellulenavgauche/cellulenavgauche.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import * as menuGauche from '../../assets/menutest.json';
 
 @Component({
   selector: 'app-cellulenavgauche',
@@ -6,11 +7,46 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./cellulenavgauche.component.css', '../../app/app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class CellulenavgaucheComponent implements OnInit {
+export class CellulenavgaucheComponent implements OnInit, AfterViewInit{
+
+
+
+  menujson: any = (menuGauche as any).default ; 
 
   constructor() { }
 
-  ngOnInit() {
+  private displayMenu():void {
+   // let titregauche : any  = this.menujson[0].entete[0].titregauche ;
+    let labeltree : any  = this.menujson;
+    console.log("<CellulenavgaucheComponent> - displayMenu()");
+    console.log("labeltree",labeltree);
+
+    for (let key in labeltree) {
+      let value = labeltree[key];
+      console.log("labeltree[key]",labeltree[key]);
+    }
+
+    for (let key in labeltree) {
+      console.log("strEleMenu[i]", labeltree[key]) ;
+      let objMenu = labeltree[key] ; 
+      
+      for (let key in objMenu) {
+        console.log("key", key);
+        if (key=="listemenugauche")  //on est sur le noeud d'information menu gauche
+            console.log("on est sur listemenugauche");   
+      }      
+    }
   }
+
+  ngAfterViewInit(): void {
+    console.log("<CellulenavgaucheComponent> - ngAfterViewInit()");
+    this.displayMenu();
+  }
+
+
+  ngOnInit() {
+    console.log("<CellulenavgaucheComponent> - ngOnInit()");
+  }
+  
 
 }
